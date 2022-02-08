c53ae73de0b66d9f7c8bc9998a38f4b248cfb400
wendy winata <winatawendy128@gmail.com>
2022-02-08 19:59:36 +0800

    first commit

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav-menu py-4 px-16 flex justify-between items-center w-full">
+    <h1 class="py-4 logo text-xl font-semi">DolanTravel.</h1>
+
+    <ul class="flex gap-8">
+      <li>Our Services</li>
+      <li>About Us</li>
+      <li class="special">Book Now</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.nav-menu {
+  position: relative;
+  z-index: 2;
+}
+
+li {
+  padding: 4px 16px;
+}
+li.special {
+  background: rgb(110, 30, 240);
+  border-radius: 20px;
+}
+</style>
+
+<script>
+export default {
+  name: "navigation",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <navigation-menu> </navigation-menu>
+    <div class="hero py-16 px-16">
+      <h1 class="mb-2">
+        Plan Your
+        <span :class="[hideText ? 'hide' : '']">{{
+          heroText[counterText]
+        }}</span>
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu ut pretium
+        mi eget luctus. Elementum consequat diam magnis posuere convallis.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import NavigationMenu from "../components/NavigationMenu.vue";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: { NavigationMenu },
+  data() {
+    return {
+      heroText: ["Holiday", "Memory", "Journey", "Trip"],
+      counterText: 0,
+      hideText: false,
+    };
+  },
+  methods: {
+    startTextAnimation() {
+      setInterval(() => {
+        this.hideText = true;
+        setTimeout(() => {
+          this.hideText = false;
+          this.counterText++;
+          if (this.counterText == this.heroText.length) this.counterText = 0;
+        }, 2000);
+      }, 4000);
+    },
+  },
+  mounted() {
+    this.startTextAnimation();
   },
 };
 </script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  width: 100%;
+  background-image: url("../assets/bg-home-section1.png");
+  position: relative;
+}
+
+.home::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0), #000);
+  position: absolute;
+  bottom: 0;
+}
+
+.home .hero {
+  position: relative;
+  z-index: 2;
+  align-content: center;
+  width: 700px;
+}
+
+.home .hero p {
+  color: rgba(255, 255, 255, 0.8);
+}
+
+.hero h1 {
+  font-size: 42px;
+  font-weight: bold;
+  color: white;
+  word-spacing: 8px;
+  width: 100%;
+  display: flex;
+}
+
+.hero h1 span {
+  width: 50%;
+  margin-left: 16px;
+  overflow: hidden;
+  transition: 1s cubic-bezier(0.39, 0.575, 0.565, 1);
+}
+
+.hero h1 span.hide {
+  opacity: 0%;
+  transform: translateY(-50%);
+}
+</style>
